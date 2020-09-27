dc4425f81e9ed628d87f019945304cc1c8fe3fcf
León Máximos Ramos <68385@PROWAY.TREINA>
2020-09-27 17:43:16 -0300

    etodo construtor

--- Zoologico/src/br/com/logica/zoologico/principal/Main.java
+++ Zoologico/src/br/com/logica/zoologico/principal/Main.java
@@ -6,21 +6,21 @@ public class Main {
 
 	public static void main(String[] args) {
 		// Criando uma nova instancia do objeto
-		Animal a1 = new Animal();
+		Animal a1 = new Animal("Jojo Todinho", "Gato", 1);
 		
 		// atribuindo os valores as propriedades
-		a1.setNome("jojo todinho");
-		a1.setEspecie("gato");
-		a1.setIdade(1);
+		//a1.setNome("jojo todinho");
+		//a1.setEspecie("gato");
+		//a1.setIdade(1);
 		
 		System.out.println("Nome do animal " + a1.getNome());
 		// criando uma nova instancia do objeto
-		Animal a2 = new Animal();
+		Animal a2 = new Animal("Spike", "Cachorro", 3);
 		
 		// atribuindo os valores as propriedades
-		a2.setNome("spike");
-		a2.setEspecie("cachorro");
-		a2.setIdade(6);
+		//a2.setNome("spike");
+		//a2.setEspecie("cachorro");
+		//a2.setIdade(6);
 		
 		a2.emitirSom();
 		
