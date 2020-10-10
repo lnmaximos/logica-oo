7c9b5e3ef0698bc8d225413deeb7ab2fa6e7f4e0
León Máximos Ramos <68385@PROWAY.TREINA>
2020-10-10 17:01:52 -0300

    comando para criar tabela

--- Zoologico/src/br/com/logica/script-banco.sql
+++ Zoologico/src/br/com/logica/script-banco.sql
@@ -1,2 +1,12 @@
--- Criar schema ou banco
+-- Criar schema ou banco	
 CREATE SCHEMA `zoo` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_mysql500_ci ;
+
+
+-- Script para criar uma tabela
+
+CREATE TABLE `zoo`.`animal` (
+  `codigo` INT NOT NULL AUTO_INCREMENT,
+  `especie` VARCHAR(20) NOT NULL,
+  `idade` INT NULL,
+  `estavivo` TINYINT NULL,
+  PRIMARY KEY (`codigo`));
